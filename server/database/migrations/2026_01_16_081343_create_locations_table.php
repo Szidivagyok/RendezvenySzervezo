3925b6280c3d80188fa4d5a6b95d3650ea572ebf
Szidivagyok <eszter0100@gmail.com>
2026-01-16 10:15:03 +0100

    migrate location javitas

--- server/database/migrations/2026_01_16_081343_create_locations_table.php
+++ server/database/migrations/2026_01_16_081343_create_locations_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('locationId')->constrained('orders')->onDelete('restrict');
             $table->string('cityName');
             $table->decimal('zipCode');
             $table->unique('street');
