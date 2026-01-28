ca1a9695b0843d7482596bc36525e2c13817d5eb
Adri <adriennjakab970@gmail.com>
2026-01-28 11:05:35 +0100

    Location és sima COntroller

--- server/app/Http/Controllers/LocationController.php
+++ server/app/Http/Controllers/LocationController.php
@@ -1,102 +1,71 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
-use App\Models\Location;
-use App\Http\Requests\StoreLocationRequest;
-use App\Http\Requests\UpdateLocationRequest;
+
+use App\Models\Location as CurrentModel;
+use App\Http\Requests\StoreLocationRequest as StoreCurrentModelRequest;
+use App\Http\Requests\UpdateLocationRequest as UpdateCurrentModelRequest;
 use Illuminate\Database\QueryException;
- 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 class LocationController extends Controller
 {
+    use AuthorizesRequests;
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        try {
-            $rows = Location::all();
- 
-            return response()->json([
-                'message' => 'OK',
-                'data' => $rows
-            ], 200, options: JSON_UNESCAPED_UNICODE);
- 
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => "Server error: {$e->getCode()}",
-                'data' => []
-            ], 500, options: JSON_UNESCAPED_UNICODE);
-        }
+        return $this->apiResponse(
+            function () {
+                return CurrentModel::all();
+            }
+        );
     }
- 
-    public function store(StoreLocationRequest $request)
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCurrentModelRequest $request)
     {
-        try {
-            $row = Location::create($request->all());
- 
-            return response()->json([
-                'message' => 'OK',
-                'data' => $row
-            ], 201, options: JSON_UNESCAPED_UNICODE);
- 
-        } catch (QueryException $e) {
-            return response()->json([
-                'message' => 'Insert error',
-                'data' => null
-            ], 400, options: JSON_UNESCAPED_UNICODE);
-        }
+        return $this->apiResponse(
+            function () use ($request) {
+                return CurrentModel::create($request->validated());
+            }
+        );
     }
- 
+
+    /**
+     * Display the specified resource.
+     */
     public function show(int $id)
     {
-        $row = Location::find($id);
- 
-        if ($row) {
-            return response()->json([
-                'message' => 'OK',
-                'data' => $row
-            ], 200, options: JSON_UNESCAPED_UNICODE);
-        }
- 
-        return response()->json([
-            'message' => "Not found id: $id",
-            'data' => null
-        ], 404, options: JSON_UNESCAPED_UNICODE);
+        return $this->apiResponse(function () use ($id) {
+            return CurrentModel::findOrFail($id);
+        });
     }
- 
-    public function update(UpdateLocationRequest $request, int $id)
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCurrentModelRequest $request, int $id)
     {
-        $row = Location::find($id);
- 
-        if ($row) {
-            $row->update($request->all());
- 
-            return response()->json([
-                'message' => 'OK',
-                'data' => $row
-            ], 200, options: JSON_UNESCAPED_UNICODE);
-        }
- 
-        return response()->json([
-            'message' => "Patch error. Not found id: $id",
-            'data' => null
-        ], 404, options: JSON_UNESCAPED_UNICODE);
+        return $this->apiResponse(function () use ($request, $id) {
+            $row = CurrentModel::findOrFail($id);
+            $row->update($request->validated());
+            return $row;
+        });
     }
- 
+
+    /**
+     * Remove the specified resource from storage.
+     */
+
     public function destroy(int $id)
     {
-        $row = Location::find($id);
- 
-        if ($row) {
-            $row->delete();
- 
-            return response()->json([
-                'message' => 'OK',
-                'data' => ['id' => $id]
-            ], 200, options: JSON_UNESCAPED_UNICODE);
-        }
- 
-        return response()->json([
-            'message' => "Delete error. Not found id: $id",
-            'data' => null
-        ], 404, options: JSON_UNESCAPED_UNICODE);
+        return $this->apiResponse(function () use ($id) {
+            CurrentModel::findOrFail($id)->delete();
+            return ['id' => $id];
+        });
     }
 }
