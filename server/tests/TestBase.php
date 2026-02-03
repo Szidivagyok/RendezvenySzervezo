ccd0d87132d1939e4c8458ffdba724e37acf7ca9
Szidivagyok <eszter0100@gmail.com>
2026-02-03 12:21:03 +0100

    testbase

--- server/tests/TestBase.php
+++ server/tests/TestBase.php
@@ -1,16 +1,86 @@
 <?php
-
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class TestBase extends TestCase
+ 
+namespace Tests;
+ 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+  class TestBase extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    use DatabaseTransactions;
+ 
+    protected function login(
+        string $email = 'admin@example.com',
+        string $password = '123'
+    ) {
+        $uri = '/api/users/login';
+ 
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+ 
+        $data = [
+            'email' => $email,
+            'password' => $password,
+        ];
+ 
+        return $this
+            ->withHeaders($headers)
+            ->postJson($uri, $data);
+    }
+ 
+    protected function logout(string $token)
     {
-        $this->assertTrue(true);
+        $uri = '/api/users/logout';
+ 
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => "Bearer {$token}",
+        ];
+ 
+        return $this
+            ->withHeaders($headers)
+            ->postJson($uri);
+    }
+ 
+    protected function myGetToken($response): string
+    {
+        return $response->json('data.token');
+    }
+ 
+    protected function myGet(string $uri, string $token)
+    {
+        return $this
+            ->withHeaders($this->authHeaders($token))
+            ->getJson($uri);
+    }
+ 
+    protected function myPost(string $uri, array $data, string $token)
+    {
+        return $this
+            ->withHeaders($this->authHeaders($token))
+            ->postJson($uri, $data);
+    }
+ 
+    protected function myPatch(string $uri, array $data, string $token)
+    {
+        return $this
+            ->withHeaders($this->authHeaders($token))
+            ->patchJson($uri, $data);
+    }
+ 
+    protected function myDelete(string $uri, string $token)
+    {
+        return $this
+            ->withHeaders($this->authHeaders($token))
+            ->deleteJson($uri);
+    }
+ 
+    protected function authHeaders(string $token): array
+    {
+        return [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $token,
+        ];
     }
 }
