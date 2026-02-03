4367f2418bffb65bc19f0045c2ec90826894f35f
Adri <adriennjakab970@gmail.com>
2026-02-03 13:21:13 +0100

    database test

--- server/tests/Feature/EventApiTest.php
+++ server/tests/Feature/EventApiTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\Unit\TestBase;
+use Tests\TestBase;
 
 class EventApiTest extends TestBase
 {
--- server/tests/Unit/DatabaseServiceTest.php
+++ server/tests/Unit/DatabaseServiceTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Tests\TestCase;
 
 class DatabaseServiceTest extends TestCase
 {
@@ -12,5 +14,25 @@ class DatabaseServiceTest extends TestCase
     public function test_example(): void
     {
         $this->assertTrue(true);
+    }
+
+    public static function tablesProvider(): array
+    {
+        return [
+            'services tábla' => ['services'],
+            'users tábla' => ['users'],
+            'orders tábla' => ['orders'],
+            'order_services tábla' => ['order_services'],
+            'locations tábla' => ['locations'],
+        ];
+    }
+
+    #[DataProvider('tablesProvider')]
+    public function test_exists_all_tables($table): void
+    {
+        $this->assertTrue(
+            Schema::hasTable($table),
+            "A '$table' tábla nem létezik"
+        );
     }
 }
