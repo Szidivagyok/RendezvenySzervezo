66cd05482c65ab7f98a658e4b289f44517a31176
Adri <adriennjakab970@gmail.com>
2026-02-03 11:19:15 +0100

    testfiles

---
+++ server/tests/Feature/EventApiTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Unit\TestBase;
+
+class EventApiTest extends TestBase
+{
+    /**
+     * A basic feature test example.
+     */
+     public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
