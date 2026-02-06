bb4d276a9fde3c8f3d94a3751482ca0167540658
Adri <adriennjakab970@gmail.com>
2026-02-06 09:02:50 +0100

    update location rq

--- server/app/Http/Requests/UpdateLocationRequest.php
+++ server/app/Http/Requests/UpdateLocationRequest.php
@@ -21,30 +21,62 @@ class UpdateLocationRequest extends FormRequest
      */
     public function rules(): array
     {
-         
-    $id = $this->route('id'); // vagy 'location', attól függ mi a route paraméter neve
 
-    return [
-        'zipCode' => [
-            'required',
-            'string',
-            'max:10',
-            Rule::unique('locations')
-                ->ignore($id, 'id')
-                ->where(
-                    fn ($q) => $q
-                        ->where('zipCode', request('zipCode'))
-                        ->where('street', request('street'))
-                        ->where('houseNumber', request('houseNumber'))
-                        ->where('locationName', request('locationName'))
-                ),
-        ],
+        $id = $this->route('id'); // vagy 'location', attól függ mi a route paraméter neve
 
-        'cityName' => ['required', 'string', 'max:255'],
-        'maxCapacity' => ['required', 'integer', 'min:1'],
-        'minCapacity' => ['required', 'integer', 'min:1'],
-        'priceSlashPerson' => ['required', 'decimal:0', 'min:0'],
-        'roomPriceSlashDay' => ['required', 'decimal:0', 'min:0'],
-    ];
+        return [
+            'zipCode' => [
+                'required',
+                'string',
+                'max:10',
+                Rule::unique('locations')
+                    ->ignore($id, 'id')
+                    ->where(
+                        fn($q) => $q
+                            ->where('zipCode', request('zipCode'))
+                            ->where('street', request('street'))
+                            ->where('houseNumber', request('houseNumber'))
+                            ->where('locationName', request('locationName'))
+                    ),
+            ],
+
+            'cityName.required' => 'A város megadása kötelező.',
+            'cityName.string' => 'A város neve csak szöveg lehet.',
+            'cityName.max' => 'A város neve legfeljebb 255 karakter lehet.',
+
+            'zipCode.required' => 'Az irányítószám megadása kötelező.',
+            'zipCode.string' => 'Az irányítószám csak szöveg lehet.',
+            'zipCode.max' => 'Az irányítószám legfeljebb 10 karakter lehet.',
+
+            'street.required' => 'Az utca megadása kötelező.',
+            'street.string' => 'Az utca neve csak szöveg lehet.',
+            'street.max' => 'Az utca neve legfeljebb 255 karakter lehet.',
+
+            'houseNumber.required' => 'A házszám megadása kötelező.',
+            'houseNumber.string' => 'A házszám csak szöveg lehet.',
+            'houseNumber.max' => 'A házszám legfeljebb 50 karakter lehet.',
+
+            'locationName.required' => 'A helyszín neve kötelező.',
+            'locationName.string' => 'A helyszín neve csak szöveg lehet.',
+            'locationName.max' => 'A helyszín neve legfeljebb 255 karakter lehet.',
+
+            'maxCapacity.required' => 'A maximális kapacitás megadása kötelező.',
+            'maxCapacity.integer' => 'A maximális kapacitás csak egész szám lehet.',
+            'maxCapacity.min' => 'A maximális kapacitás minimum 1 lehet.',
+
+            'minCapacity.required' => 'A minimális kapacitás megadása kötelező.',
+            'minCapacity.integer' => 'A minimális kapacitás csak egész szám lehet.',
+            'minCapacity.min' => 'A minimális kapacitás minimum 1 lehet.',
+
+            'priceSlashPerson.required' => 'Az egy főre jutó ár megadása kötelező.',
+            'priceSlashPerson.decimal' => 'Az egy főre jutó ár csak szám lehet.',
+            'priceSlashPerson.min' => 'Az egy főre jutó ár nem lehet negatív.',
+
+            'roomPriceSlashDay.required' => 'A napi teremár megadása kötelező.',
+            'roomPriceSlashDay.decimal' => 'A napi teremár csak szám lehet.',
+            'roomPriceSlashDay.min' => 'A napi teremár nem lehet negatív.',
+
+            'zipCode.unique' => 'Ezen a címen már létezik helyszín.', // összetett unique üzenet
+        ];
     }
 }
