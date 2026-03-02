216841eebf7f8b59f757c5a7eb71d5904b51e5b1
Adri <adriennjakab970@gmail.com>
2026-03-02 09:26:42 +0100

    exampletest

--- server/tests/Feature/ExampleTest.php
+++ server/tests/Feature/ExampleTest.php
@@ -3,17 +3,44 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use Tests\TestBase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends TestBase
 {
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/api/x');
+        //dd($response);
         $response->assertStatus(200);
+        $response->assertSee('API');
+    }
+
+    public function test_user_login_logout()
+    {
+        //login admin
+        $response = $this->login('megrendelo@example.com', '123');
+        $response->assertStatus(200);
+
+        //token
+        $token = $this->myGetToken($response);
+
+        //get
+        $uri = '/api/users';
+        $response = $this->myGet($uri, $token);
+        //2xx: minden oké
+        //4xx: klienshiba
+        //5xx: serverhiba
+        // $response->assertStatus(403);
+       //$response->assertSuccessful();
+       $response->assertClientError();
+
+        //logout
+        $response = $this->logout($token);
+        $response->assertStatus(200);
+
+
     }
 }
