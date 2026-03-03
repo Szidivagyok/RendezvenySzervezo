7a76bb2f0a4289a359a4b544d94a1cc0e2cb11d1
Adri <adriennjakab970@gmail.com>
2026-03-03 09:56:31 +0100

    valami

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,7 +3,29 @@
     <h1>Home</h1>
     <div class="alert alert-primary" role="alert">
       <i class="bi bi-airplane-engines"></i>
-        A simple primary alert—check it out!
+      A simple primary alert—check it out!
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label"
+        >Email address</label
+      >
+      <input
+        type="email"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label"
+        >Example textarea</label
+      >
+      <textarea
+        class="form-control"
+        id="exampleFormControlTextarea1"
+        rows="3"
+      ></textarea>
     </div>
   </div>
 </template>
